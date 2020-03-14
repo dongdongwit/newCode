9587f0813c0ec1f9c6967b0e3d27fa2c70425a26
dongdongwit <296767482@qq.com>
2020-03-14 11:39:14 +0800

    [core] add binarySearch

---
+++ src/main/java/nowcoder/com/Find.java
@@ -0,0 +1,37 @@
+package nowcoder.com;
+
+public class Find {
+    public static int recursionBinarySearch(int[] arr, int key, int low, int high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[mid] < key) {
+            return recursionBinarySearch(arr, key, mid + 1, high);
+        } else if (arr[mid] > key) {
+            return recursionBinarySearch(arr, key, low, mid - 1);
+        } else {
+            return -1;
+        }
+    }
+
+    public static int binarySearch(int[] arr, int key, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > key) {
+                high = mid - 1;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else if (arr[mid] == key) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] arg) {
+        int[] arr = {1,3,5,7,9,11,23,44,66};
+//        System.out.println("result: " + recursionBinarySearch(arr, 11, 0, arr.length - 1));
+        System.out.println("result: " + binarySearch(arr, 11, 0, arr.length - 1));
+    }
+}
---
+++ src/main/java/nowcoder/com/GraphAlg.java
@@ -0,0 +1,16 @@
+package nowcoder.com;
+
+public class GraphAlg {
+    /**
+     * reference：http://data.biancheng.net/view/40.html
+     * 普里姆(prim)算法:查找多节点之间的最小生成树--不产生新的节点
+     * 将节点分为两类，一类是在查找过程中已包含在树中的(假设为A类)，剩下的另一类是B类
+     * 对于给定的连通图，起始状态全部归为B类，选定任意一个顶点作为起始点，将其从B类移动到A类中，
+     * 找出B类中到该顶点的最短路径节点，转移到A类中
+     */
+
+    /**
+     * Steiner Tree，基于动态规划的思想，找出能覆盖输入节点的最小子图
+     */
+
+}
