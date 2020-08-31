d775acd8d5156ce928c77396957ea53fb92077f9
源进 <wp256815@alibaba-inc.com>
2020-08-31 12:06:54 +0800

    [feature] 修正pom版本为1.8，Find模块新增二分查找应用--求平方

--- src/main/java/newcoder/com/Find.java
+++ src/main/java/newcoder/com/Find.java
@@ -29,11 +29,44 @@ public class Find {
         return -1;
     }
 
+    public static double orgSqrt(double c)
+    {
+        if (c < 0) return Double.NaN;
+        double err = 1e-15;
+        double t = c;
+        while (Math.abs(t - c/t) > err * t)
+            t = (c/t + t) / 2.0;
+        return t;
+    }
+
+    public static double sqrt(double input) {
+        double mid = Double.NaN;
+        if (input >= 0) {
+            double low = input > 1 ? 1 : input, high = input > 1 ? input : 1;
+            mid = (low + high) / 2;
+            while (Math.abs(mid*mid - input) > 1e-15) {
+//                大了,最大值减小
+                if ((mid*mid - input) > 0) {
+                    high = mid;
+                } else {
+                    low = mid;
+                }
+                mid = (low + high) / 2;
+            }
+        }
+        return mid;
+    }
+
     public static void main(String[] arg) {
-        int[] arr = {1,3,5,7,9,11,23,44,66};
-        String s = "asb";
+        System.out.println(String.format("result: %s", sqrt(16)));
+        System.out.println(String.format("result: %s", sqrt(1)));
+        System.out.println(String.format("result: %s", sqrt(0.25)));
+        System.out.println(String.format("result: %s", sqrt(0)));
+        System.out.println(String.format("result: %s", sqrt(-4)));
+//        int[] arr = {1,3,5,7,9,11,23,44,66};
+//        String s = "asb";
 //        s
 //        System.out.println("result: " + recursionBinarySearch(arr, 11, 0, arr.length - 1));
-        System.out.println("result: " + binarySearch(arr, 11, 0, arr.length - 1));
+//        System.out.println("result: " + binarySearch(arr, 11, 0, arr.length - 1));
     }
 }
